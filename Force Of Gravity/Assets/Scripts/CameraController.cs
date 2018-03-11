577a98c553ab6dcf7bd37024766dc986a535a758
TurtleGameDev <theturtleofawesomeness@gmail.com>
2018-03-11 12:12:18 -0700

    Added settings page

--- Force Of Gravity/Assets/Scripts/CameraController.cs
+++ Force Of Gravity/Assets/Scripts/CameraController.cs
@@ -2,27 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
     public GameObject Player;
     Vector2 currentVel = Vector2.zero;
-
-    bool following = false;
     private void Update()
     {
-        if (transform.position.y > -0)
-        {
-            transform.position = Vector2.SmoothDamp(transform.position, Player.transform.position, ref currentVel, 0.1f);
-            transform.position = new Vector3(transform.position.x, transform.position.y, -10);
-        }
-        else
-        {
-            transform.position = Vector2.SmoothDamp(transform.position, Player.transform.position, ref currentVel, 0.03f);
-            transform.position = new Vector3(transform.position.x, transform.position.y, -10);
-            if (Vector2.Distance(transform.position, Player.transform.position) < 1 || following)
-            {
-                following = true;
-                transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y, -10);
-            }
-        }
+        transform.position = Vector2.SmoothDamp(transform.position, Player.transform.position, ref currentVel, 0.1f);
+        transform.position = new Vector3(transform.position.x, transform.position.y, -10);
     }
 }
--- Force Of Gravity/Assets/Scripts/PauseController.cs
+++ Force Of Gravity/Assets/Scripts/PauseController.cs
@@ -17,4 +17,33 @@ public class PauseController : MonoBehaviour
     {
         camera.GetComponent<Camera>().orthographicSize = 4;
     }
+
+    public bool Paused = false;
+    public GameObject PauseMenu;
+    private void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            if (Paused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1;
+        Paused = false;
+    }
+    public void Pause()
+    {
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0;
+        Paused = true;
+    }
 }
