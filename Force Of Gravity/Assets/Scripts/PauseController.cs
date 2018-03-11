7b63a65b0ac583c5cbd5f93971211c79deb6afa9
TurtleGameDev <theturtleofawesomeness@gmail.com>
2018-03-11 09:33:19 -0700

    Added a Basic Pause Menu

---
+++ Force Of Gravity/Assets/Scripts/PauseController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseController : MonoBehaviour
+{
+    public new GameObject camera;
+    public void EasyZoom()
+    {
+        camera.GetComponent<Camera>().orthographicSize = 12;
+    }
+    public void MidZoom()
+    {
+        camera.GetComponent<Camera>().orthographicSize = 8;
+    }
+    public void HardZoom()
+    {
+        camera.GetComponent<Camera>().orthographicSize = 4;
+    }
+}
---
+++ Force Of Gravity/Assets/Scripts/PauseMenuOpener.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuOpener : MonoBehaviour {
+    public bool Paused = false;
+    public GameObject PauseMenu;
+    private void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            if (Paused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1;
+        Paused = false;
+    }
+    public void Pause()
+    {
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0;
+        Paused = true;
+    }
+}
