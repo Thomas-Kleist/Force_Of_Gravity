adb9c8b414f1d8c2cc1ef07482f7ec4249916156
TurtleGameDev <theturtleofawesomeness@gmail.com>
2018-03-12 18:42:39 -0700

    Added a portal

---
+++ Force Of Gravity/Assets/Scripts/Portal.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject OtherPortal;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.transform.gameObject == player) player.transform.position = OtherPortal.transform.position  - new Vector3(0, 3f, 0);
+    }
+}
